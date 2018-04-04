522e059a93f5c004392804cae045ebbad270cf05
david salas montalvan <david.salas@atiperu.com>
2018-04-04 09:48:49 -0500

    dao v5

--- src/com/ctac/dao/VisitDAOImplements.java
+++ src/com/ctac/dao/VisitDAOImplements.java
@@ -1260,7 +1260,7 @@ VALUES ('2018-03-14 08:00:00',3,1,1,now(),1,3,1,1) RETURNING call_cod;
 					"  inner join visits.department f on f.id_department=a.id_department \n" + 
 					"  inner join visits.occupation g on g.id_occupation=c.id_occupation\n" + 
 					"  where \n" + 
-					"  a.status<>3 \n" + 
+					"  a.status not in (3,4) \n" + 
 					"  and \n" + 
 					"  cast( :datev as date) BETWEEN cast(a.date_ini as date) AND cast(a.date_end as date) \n" + 
 					"  and	 \n" + 
