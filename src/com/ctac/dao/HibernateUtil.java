ce12c29f4ea9656c7e1215a0bf1a7493255814a9
dasamo <dasamo.ntic@gmail.com>
2018-04-21 23:00:00 -0500

    resources

--- src/com/ctac/dao/HibernateUtil.java
+++ src/com/ctac/dao/HibernateUtil.java
@@ -1,9 +1,9 @@
 package com.ctac.dao;
 
-import java.io.File;
+
 import org.apache.log4j.Logger;
 import org.hibernate.SessionFactory;
-import org.hibernate.cfg.Configuration;
+import org.hibernate.cfg.AnnotationConfiguration;
 
 public class HibernateUtil {
 	private static SessionFactory sessionFactory = buildSessionFactory();
@@ -12,7 +12,8 @@ public class HibernateUtil {
 	private static SessionFactory buildSessionFactory() {
 		try {
 			//return (new Configuration()).configure(new File("/opt/smsmasivo/etc/hibernate.cfg.xml"))
-			return (new Configuration()).configure("/resources/hibernate.cfg.xml").buildSessionFactory();
+			//return (new Configuration()).configure("/resources/hibernate.cfg.xml").buildSessionFactory();
+			return new AnnotationConfiguration().configure().buildSessionFactory();
 		} catch (Throwable arg0) {
 			System.err.println("Initial SessionFactory creation failed." + arg0);
 			throw new ExceptionInInitializerError(arg0);
